7812b04e2b4e64d6a454df828fea67b6c431794d
pradipdy65 <iit2022065@iiita.ac.in>
2025-02-03 22:17:50 +0530

    github data added

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const express = require('express')
 
 const app = express()
 const port = 4000
+const githubData={
+    "message": "Not Found",
+    "documentation_url": "https://docs.github.com/rest",
+    "status": "404"
+  }
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -15,7 +20,9 @@ app.get('/twitter',(req,res)=>{
 app.get('/login',(req,res)=>{
     res.send("Welcome to our page!")
 })
-
+app.get('/github',(req,res)=>{
+    res.json(githubData)
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
